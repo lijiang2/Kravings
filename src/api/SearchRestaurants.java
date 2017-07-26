05cadc6b7a46491f4d59d8f27ea09357033939e9
Li Jiang <tudouxiaohei2@gmail.com>
2017-07-26 00:42:37 -0500

    Merge pull request #2 from lijiang2/develop
    Develop - YelpAPI simple query

--- src/api/SearchRestaurants.java
+++ src/api/SearchRestaurants.java
@@ -13,6 +13,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import db.DBConnection;
+import db.MySQLDBConnection;
+
 /**
  * Servlet implementation class SearchRestaurants
  */
@@ -31,24 +34,21 @@ public class SearchRestaurants extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
 		JSONArray array = new JSONArray();
-		try {
-			if (request.getParameterMap().containsKey("user_id")
-					&& request.getParameterMap().containsKey("lat")
-					&& request.getParameterMap().containsKey("lon")) {
-				String userId = request.getParameter("user_id");
-				double lat = Double.parseDouble(request.getParameter("lat"));
-				double lon = Double.parseDouble(request.getParameter("lon"));
-				// fake restaurant info
-				array.put(new JSONObject().put("name", "Lao Sze Chuan"));
-				array.put(new JSONObject().put("name", "Gyu-Kaku"));
-			}
-		} catch (JSONException e) {
-			e.printStackTrace();
+		DBConnection connection = new MySQLDBConnection();
+		if (request.getParameterMap().containsKey("lat")
+				&& request.getParameterMap().containsKey("lon")) {
+			// term is null or empty by default
+			String term = request.getParameter("term");
+            String userId = "1111";
+			double lat = Double.parseDouble(request.getParameter("lat"));
+			double lon = Double.parseDouble(request.getParameter("lon"));
+			array = connection.searchRestaurants(userId, lat, lon, term);
 		}
 		RpcParser.writeOutput(response, array);
-	}
+    }
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
